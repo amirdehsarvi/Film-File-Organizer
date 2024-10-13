bcaf13187a4bd3af0733d9c68e2e62e3f416f5a0
Amir Dehsarvi <amir.dehsarvi@gmail.com>
2024-10-13 10:45:25 +0200

    added the filename in for the user to see

--- interactive_movie_sorter.py
+++ interactive_movie_sorter.py
@@ -1,94 +1,11 @@
-import os
-import re
-from imdb import IMDb
-import readline
-import glob
-
-# Enable tab completion for folder paths
-def complete_path(text, state):
-    return (glob.glob(text + '*') + [None])[state]
-
-readline.set_completer_delims('')
-readline.parse_and_bind("tab: complete")
-readline.set_completer(complete_path)
-
-# Function to clean and format movie name from the file name
-def clean_movie_name(file_name):
-    movie_name = os.path.splitext(file_name)[0]
-    patterns_to_remove = [
-        r'\b\d{3,4}p\b', r'\bBluRay\b', r'\bWEB\b', r'\bHD\b', r'\bSD\b',
-        r'\bAAC\b', r'\bx264\b', r'\bx265\b', r'\bXviD\b', r'\bHDRip\b',
-        r'\bDVDRip\b', r'\bBRRip\b', r'\bH264\b', r'\b10bit\b', r'\b8bit\b',
-        r'\bHEVC\b', r'\b@lubokvideo\b', r'\bSoftSub\b', r'\bFW\b', r'\bDream\b',
-        r'\bVeDeTT\b', r'\bAvaMovie\b'
-    ]
-    for pattern in patterns_to_remove:
-        movie_name = re.sub(pattern, '', movie_name, flags=re.IGNORECASE)
-    movie_name = re.sub(r'[_\.]', ' ', movie_name)
-    movie_name = re.sub(r'\s+', ' ', movie_name).strip()
-    movie_name = re.sub(r'\b\d{4}\b.*$', '', movie_name).strip()
-    return movie_name
-
-# Function to get movie data from IMDb
-def get_movie_data(movie_name):
-    ia = IMDb()
-    search_results = ia.search_movie(movie_name)
-    if search_results:
-        return ia.get_movie(search_results[0].movieID)  # Return the first search result
-    return None
-
-# Function to get movie data by IMDb ID
-def get_movie_data_by_id(imdb_id):
-    ia = IMDb()
-    imdb_id = imdb_id.replace('tt', '')  # Remove the 'tt' prefix
-    return ia.get_movie(imdb_id)
-
-# Function to create directory structure and move files
-def organize_movie(file, movie_data, folder_path):
-    directors = movie_data.get('director', [])
-    director_names = ', '.join(director['name'] for director in directors) if directors else "Unknown"
-    release_year = movie_data.get('year', 'Unknown')
-    movie_name = movie_data.get('title', 'Unknown')
-
-    dir_structure = os.path.join(folder_path, director_names, f"{release_year} - {movie_name}")
-
-    if not os.path.exists(dir_structure):
-        os.makedirs(dir_structure)
-
-    source_file_path = file
-    dest_file_path = os.path.join(dir_structure, os.path.basename(file))
-
-    os.rename(source_file_path, dest_file_path)
-
-    new_file_name = f"{movie_name}{os.path.splitext(file)[1]}"
-    os.rename(dest_file_path, os.path.join(dir_structure, new_file_name))
-
-    # Delete the now empty folders
-    parent_dir = os.path.dirname(source_file_path)
-    while parent_dir != folder_path and not os.listdir(parent_dir):
-        os.rmdir(parent_dir)
-        parent_dir = os.path.dirname(parent_dir)
-
-# Function to get all files in the folder, including subfolders
-def get_all_files(folder_path):
-    all_files = []
-    for root, _, files in os.walk(folder_path):
-        for file in files:
-            all_files.append(os.path.join(root, file))
-    return all_files
-
-# Get folder path from user with auto-completion
-folder_path = input("Please enter the folder path (tab to auto-complete): ")
-
-# Get list of all files in the folder and subfolders
-files = get_all_files(folder_path)
-
 # Iterate through each file in the folder
 for file in files:
     if os.path.basename(file).startswith('.'):
         continue
 
-    movie_name = clean_movie_name(os.path.basename(file))
+    original_file_name = os.path.basename(file)
+    movie_name = clean_movie_name(original_file_name)
+    print(f"Original file: {original_file_name}")
     print(f"Searching for movie: {movie_name}")
 
     movie_data = get_movie_data(movie_name)
